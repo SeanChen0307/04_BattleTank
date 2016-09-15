dbf2bbfcc75424ab29babcfa6bd1f78e6da7cb82
DESKTOP-5IENGRI\Sean <85805631@qq.com>
2016-09-15 21:37:57 +0800

    Add a sub aiming component to tank class

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,8 +2,9 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // Put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -12,6 +13,11 @@ class BATTLETANK_API ATank : public APawn
 
 public:
 	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimngComponent* TankAimingComponent = nullptr;
+
+
 
 private:
 	// Sets default values for this pawn's properties
