d8ce760fa7be5abb9122f23654b127bfe59c6696
DESKTOP-5IENGRI\Sean <85805631@qq.com>
2016-10-17 01:13:20 +0800

    Get better component architecture

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -16,7 +16,6 @@ ATank::ATank()
 
 	// No need to protect points as added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimingComponent"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("MovementComponent"));
 }
 
 // Called when the game starts or when spawned
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -20,7 +20,7 @@ public:
 	
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-		float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g acceleration
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g acceleration
 	
 	
 };
